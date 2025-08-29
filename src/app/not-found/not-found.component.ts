547ef7ef125cc388429d1e9d1c48b4791b2b23ab
Jirapongse Phuriphanvichai <jirapongse.phuriphanvichai@lseg.com>
2025-08-29 14:00:01 +0700

    Add files via upload

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
+  standalone: true,
+   imports: [MatIconModule,MatCardModule,RouterModule],
   templateUrl: './not-found.component.html',
   styleUrls: ['../app.component.scss']
 })
