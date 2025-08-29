00fc44a98a9f7fc40f6d5f9b81ac16daca15a73e
Jirapongse Phuriphanvichai <jirapongse.phuriphanvichai@lseg.com>
2025-08-29 13:59:15 +0700

    Add files via upload

--- src/app/esgdata/esgdata.component.ts
+++ src/app/esgdata/esgdata.component.ts
@@ -1,9 +1,15 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-
+import {MatCardModule} from '@angular/material/card';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table'  
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-esgdata',
+  standalone: true,
+  imports: [MatCardModule,FormsModule,ReactiveFormsModule,MatInputModule,MatTableModule,CommonModule],
   templateUrl: './esgdata.component.html',
   styleUrls: ['./esgdata.component.scss']
 })
